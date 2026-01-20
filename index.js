eae06bfdc83348c7d661887b266faccfab3f240b
saibakhan7771-cmd <saibakhan7771@gmail.com>
2026-01-20 23:18:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ app.post("/chat", async (req, res) => {
         model: "openai/gpt-oss-20b",
         temperature: 0.9,
         top_p: 0.9,
+        max_tokens: 150,
         messages: [
           {
             role: "system",
